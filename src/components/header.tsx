fe4fca2cce3ea6e95d1d80cb9539ebdfc420066c
Le22 <louis-etienne.girard@epitech.eu>
2024-11-12 16:58:32 +0100

    add action git

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
 const Header = () => {
   return (
     <header className="w-full p-10 absolute top-0 flex justify-between items-center z-10 text-white">
-      <p className="text-xl font-bold">Aurora Home</p>
+      <div className="flex gap-4 items-center">
+        <img src="/logo.webp" alt="Aurora" className="w-10 h-10" />
+        <p className="text-xl font-bold">Aurora Home</p>
+      </div>
       <nav className="space-x-2">
         <a>Contact</a>
       </nav>
