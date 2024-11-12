613b1d3773994a7d0d1c9b48ec9c746389c20911
Le22 <louis-etienne.girard@epitech.eu>
2024-11-12 21:33:56 +0100

    fix logo url

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="w-full p-10 absolute top-0 flex justify-between items-center z-10 text-white">
       <div className="flex gap-4 items-center">
-        <img src="/logo.webp" alt="Aurora" className="w-10 h-10" />
+        <img src="logo.webp" alt="Aurora" className="w-10 h-10" />
         <p className="text-xl font-bold">Aurora Home</p>
       </div>
       <nav className="space-x-2">
